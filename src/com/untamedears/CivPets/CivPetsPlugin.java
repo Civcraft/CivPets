227ef8899f5cd0d56351e69378fa9d590139978c
Dr. Andrew Jawa <drjawa@users.noreply.github.com>
2014-07-29 15:48:30 -0400

    Remove unused imports

--- src/com/untamedears/CivPets/CivPetsPlugin.java
+++ src/com/untamedears/CivPets/CivPetsPlugin.java
@@ -1,21 +1,9 @@
 package com.untamedears.CivPets;
 
-import java.util.HashMap;
-import java.util.Random;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Ocelot;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Wolf;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CivPetsPlugin extends JavaPlugin {
